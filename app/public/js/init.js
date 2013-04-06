e7e8471167f138e0f46bbf520fa9f2fcd63fa7f4
Jon Maddox <jon@jonmaddox.com>
2013-04-05 23:06:35 -0400

    capture clicks for later

---
+++ app/public/js/init.js
@@ -0,0 +1,16 @@
+$(document).ready(function($) {
+
+  $(".channel").click(function() {
+    self = $(this)
+    channel = self.data('channel')
+
+
+
+
+    console.log()
+    return false;
+  });
+
+
+
+});
