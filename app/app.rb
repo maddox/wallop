4e061c7b896ed22805be85f1c1921166d6589dda
Aman Gupta <aman@tmm1.net>
2013-09-28 00:04:07 -0700

    clean up snapshots after 60s

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,7 @@
 require 'app/boot'
 require 'sinatra/base'
 require 'async_sinatra'
+require 'fileutils'
 
 module Wallop
   class App < Sinatra::Base
@@ -117,6 +118,7 @@ module Wallop
     aget '/channels/:channel.:timestamp.png' do
       Wallop.snapshot(params[:channel]) do |file|
         async_schedule{ cache_control :no_cache; redirect "/snapshots/#{file}" }
+        EM.add_timer(60){ FileUtils.rm_f "app/public/snapshots/#{file}" }
       end
     end
 
